b66337efdf5d9fe2614581202f32360362387be9
“Sphinxtar” <sphinxtar@gmail.com>
2021-05-07 14:33:36 -0700

    modified:   sweepDeez.py

--- sweepDeez.py
+++ sweepDeez.py
@@ -4,28 +4,29 @@
 # checks hostname:8142 and gets system status from client
 # builds XML tree of statuses in memory 
 # applies deezNetz.xsl and writes /var/www/html/deezNetz.js
+#	sock.settimeout(8)
 import socket
+port = 8142
+msg = "OK\r\n\r\n"
+ok = str.encode(msg)
 
 def sweepDeez():
-	port = 8142
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.settimeout(8)
-	doc = open('/usr/share/deez/deezhosts', 'r')
-	for line in doc:    
-		host = line.strip('\n')
-		print(host)
-		addr = socket.getaddrinfo(host, None, socket.AF_INET)
-		print(addr[1][4][0])
-		result = sock.connect_ex((addr[1][4][0], port))
-		if result == 0:
-			print("connected to "+host, port)
-			sock.send("OK".encode())
-			statstr = sock.recv(256)
-			print(statstr.decode())
-		else:
-			print("connection failed to "+host, port)
-	sock.close()
-	doc.close()
-
+    doc = open('/usr/share/deez/deezhosts', 'r')
+    for line in doc:    
+        host = line.strip('\n')
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect((host, 8142))
+            sock.sendall(ok)
+            while(True):
+                data = sock.recv(1024)
+                if(data==b''):
+                    break
+                print(data.decode().strip("\n"))
+        except Exception as ex:
+            print("<?xml version=\"1.0\"?><host name=\"" +host+"\"><status>RED</status></host>")
+            # print(host+":", ex)
+        sock.close()
+    doc.close()
 
 sweepDeez()
