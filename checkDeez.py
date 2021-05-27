33ec89a82eeab625da9c3da0d5e7ebbf2ecb7a04
“Sphinxtar” <sphinxtar@gmail.com>
2021-05-26 19:12:42 -0700

    added memory free percentage

--- checkDeez.py
+++ checkDeez.py
@@ -13,21 +13,21 @@ import urllib3
 
 url = sys.stdin.readline()
 while(True):
-    header = sys.stdin.readline()
-    if header == "\r\n":
-        break
+	header = sys.stdin.readline()
+	if header == "\r\n":
+		break
 
 cmd = url.split()[1][1:]
 
 if cmd == "OK":
-    report = 0
+	report = 0
 elif cmd == "ERR":
-    report = 1
+	report = 1
 elif cmd == "FULL":
-    report = 2
+	report = 2
 else:
-    sys.stdout.write("HTTP/1.1 404 NOT FOUND\r\n");
-    quit()
+	sys.stdout.write("HTTP/1.1 404 NOT FOUND\r\n");
+	quit()
 
 condition = 0 # 0 = GREEN 1 = RED error condition returned on OK cmd 
 stamp = time.asctime(time.gmtime())+" GMT"
@@ -40,116 +40,133 @@ http = urllib3.PoolManager(timeout=3.0)
 head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
 
+def memfree():
+	highwater = 15; # set this to percentage left to alert on
+	fm = (str(subprocess.check_output(["free", "-k"]), 'utf-8')).split('\n')
+	freemem = iter(fm)
+	next(freemem)
+	for row in freemem:
+		mlist = row.split()
+		break
+	free = int(mlist[6])/int(mlist[1])*100
+#	print(str(int(mlist[6]))+" / "+str(int(mlist[1]))+"*100="+str(free)+"%")
+	if (round(free)<highwater):
+		print("<freemem>"+str(round(free,2))+"%</freemem>")
+		condition = 1
+
+
 def diskusage():
-    global condition
-    highwater = 85 # set this to percentage full to alert on
-    diskfull = 0
-    retval = "<disk>"
-    df = (str(subprocess.check_output(["df", "-h"]), 'utf-8')).split('\n')
-    dfree = iter(df)
-    next(dfree)
-    for row in dfree:
-        cols = row.split( )
-        if len(cols) > 3:
-            if int(cols[4].strip('%')) > highwater:
-                retval = retval + "<full>"+cols[0]+" at "+cols[4]+"</full>"
-                diskfull = 1
-    retval = retval + "</disk>"
-    if diskfull > 0:
-        condition = 1
-    if report == 0 or condition != 1:
-        retval = ""
-    return(retval)
+	global condition
+	highwater = 85 # set this to percentage full to alert on
+	diskfull = 0
+	retval = "<disk>"
+	df = (str(subprocess.check_output(["df", "-h"]), 'utf-8')).split('\n')
+	dfree = iter(df)
+	next(dfree)
+	for row in dfree:
+		cols = row.split( )
+	if len(cols) > 3:
+		if int(cols[4].strip('%')) > highwater:
+			retval = retval + "<full>"+cols[0]+" at "+cols[4]+"</full>"
+			diskfull = 1
+	retval = retval + "</disk>"
+	if diskfull > 0:
+		condition = 1
+	if report == 0 or condition != 1:
+		retval = ""
+	return(retval)
 
 
 def checkport(port):
-    global condition
-    retval = "DOWN"
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(8000)
-    result = sock.connect_ex((address, port))
-    if result == 0:
-        retval = "UP"
-    else:
-        condition = 1
-    sock.close()
-    return(retval)
+	global condition
+	retval = "DOWN"
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.settimeout(8000)
+	result = sock.connect_ex((address, port))
+	if result == 0:
+		retval = "UP"
+	else:
+		condition = 1
+	sock.close()
+	return(retval)
 
 
 # web service see if we get a 200
 def checklink(link):
-    global condition
-    retval = "unknown"
-    try:
-        if link[0:4].lower() == "https":
-            response = https.request('GET',link,fields=None, headers=head)
-        else:
-            response = http.request('GET',link)	
-            if response.status != 200:
-                condition = 1
-        retval = str(response.status)
-    except requests.exceptions.ConnectionError:
-        retval = "unreachable"
-    except requests.exceptions.SSLError:
-        retval = "bad_ssl_cert"
-    return(retval)
+	global condition
+	retval = "unknown"
+	try:
+		if link[0:4].lower() == "https":
+			response = https.request('GET',link,fields=None, headers=head)
+		else:
+			response = http.request('GET',link)	
+		if response.status != 200:
+			condition = 1
+		retval = str(response.status)
+	except urllib3.exceptions.ConnectionError:
+		retval = "unreachable"
+	except urllib3.exceptions.SSLError:
+		retval = "bad_ssl_cert"
+	except:
+		retval = "connect_fail"
+	return(retval)
 
 
 def checkDeez():
-    output=[]
-    firstserve = 0
-    output.append("<?xml version=\"1.0\"?><host name=\""+hostname+"\""+" stamp=\""+stamp+"\">")
-    tree = ET.parse(file)
-    for elem in tree.iter():
-        if elem.tag == "host":
-            continue
+	output=[]
+	firstserve = 0
+	output.append("<?xml version=\"1.0\"?><host name=\""+hostname+"\""+" stamp=\""+stamp+"\">")
+	tree = ET.parse(file)
+	for elem in tree.iter():
+		if elem.tag == "host":
+			continue
 
-        if elem.tag == "service":
-            if firstserve == 0: 
-                if report > 1:
-                    output.append("<service>")
-                firstserve=1
-            else:
-                if report > 1:
-                    output.append("</service><service>")
-            continue
+		if elem.tag == "service":
+			if firstserve == 0: 
+				if report > 1:
+					output.append("<service>")
+					firstserve=1
+			else:
+				if report > 1:
+					output.append("</service><service>")
+			continue
 
-        if elem.tag == "url":
-            status = checklink(elem.text)
-            if report > 0:
-                if (status != "200" and report > 0) or report == 2:
-                    output.append("<url err=\""+status+"\">"+str(elem.text)+"</url>")
-            continue
+		if elem.tag == "url":
+			status = checklink(elem.text)
+			if report > 0:
+				if (status != "200" and report > 0) or report == 2:
+					output.append("<url err=\""+status+"\">"+str(elem.text)+"</url>")
+			continue
 
-        if elem.tag == "port":
-            uprdown = checkport(int(elem.text))
-            if (uprdown != "UP" and report > 0) or report == 2:
-                output.append("<port status=\""+uprdown+"\">"+str(elem.text)+"</port>")
-            continue
+		if elem.tag == "port":
+			uprdown = checkport(int(elem.text))
+			if (uprdown != "UP" and report > 0) or report == 2:
+				output.append("<port status=\""+uprdown+"\">"+str(elem.text)+"</port>")
+			continue
 
-        if elem.text != "None":
-            if report > 1:
-                output.append("<"+str(elem.tag)+">"+str(elem.text)+"</"+str(elem.tag)+">")
-            continue
-        else:
-            if report > 1:
-                output.append("<"+str(elem.tag)+">")
-
-    if report > 1:
-        output.append("</service>")
-    output.append(diskusage())
-    if report == 0:
-        output.append("<status>")
-        if condition != 0:
-            output.append("RED")
-        else:
-            output.append("GREEN")
-        output.append("</status>")
-    if condition == 0 and report == 1:
-        output.append("<status>No Errors Found</status>")
-    output.append("</host>\r\n")
-    del tree
-    return(output)
+		if elem.text != "None":
+			if report > 1:
+				output.append("<"+str(elem.tag)+">"+str(elem.text)+"</"+str(elem.tag)+">")
+			continue
+		else:
+			if report > 1:
+				output.append("<"+str(elem.tag)+">")
+	if report > 1:
+		output.append("</service>")
+		output.append(diskusage())
+		output.append(memfree())
+	if report == 0:
+		output.append("<status>")
+		if condition != 0:
+			output.append("YELLOW")
+		else:
+			output.append("GREEN")
+		output.append("</status>")
+	if condition == 0 and report == 1:
+		output.append("<status>No Errors Found</status>")
+	output.append("</host>\r\n")
+	del tree
+	return(output)
 
 
 sys.stdout.write("HTTP/1.1 200 OK\r\n");
--- sweepDeez.py
+++ sweepDeez.py
@@ -15,64 +15,60 @@ http = urllib3.PoolManager(timeout=3.0)
 
 
 def sweepDeez():
-    red = 0
-    yellow = 0
-    green = 0
-    reddoc = ["<?xml version=\"1.0\"?><hosts><bg>red</bg><msg>OK</msg>"]
-    yellowdoc = ["<?xml version=\"1.0\"?><hosts><bg>yellow</bg><msg>ERR</msg>"]
-    greendoc = ["<?xml version=\"1.0\"?><hosts><bg>green</bg><msg>FULL</msg>"]
+	red = 0
+	yellow = 0
+	green = 0
+	reddoc = ["<?xml version=\"1.0\"?><hosts><bg>red</bg><msg>OK</msg>"]
+	yellowdoc = ["<?xml version=\"1.0\"?><hosts><bg>yellow</bg><msg>ERR</msg>"]
+	greendoc = ["<?xml version=\"1.0\"?><hosts><bg>green</bg><msg>FULL</msg>"]
 #   rewrite to a host node
-    hoststyle = open('host.xsl')
-    hostroot = hoststyle.read()
-    hoststyle.close()
-    hostxslt = ET.XML(hostroot)
-    hosttransform = ET.XSLT(hostxslt)
+	hoststyle = open('host.xsl')
+	hostroot = hoststyle.read()
+	hoststyle.close()
+	hostxslt = ET.XML(hostroot)
+	hosttransform = ET.XSLT(hostxslt)
 #   just the status color
-    statusstyle = open('status.xsl')
-    statusroot = statusstyle.read()
-    statusstyle.close()
-    statusxslt = ET.XML(statusroot)
-    statustransform = ET.XSLT(statusxslt)
+	statusstyle = open('status.xsl')
+	statusroot = statusstyle.read()
+	statusstyle.close()
+	statusxslt = ET.XML(statusroot)
+	statustransform = ET.XSLT(statusxslt)
 
-    doc = open('/usr/share/deez/deezhosts', 'r')
-    for line in doc:    
-        host = line.strip('\n')
-        url = protocol+host+":"+str(port)+path
-        try:
-            response = http.request('GET', url)
-            if (response.status != 200):
-                # error from deez
-                status = "<?xml version=\"1.0\"?><host name=\""+host+"\"><status>YELLOW</status></host>"
-            else:
-                # success 200
-                status = response.data.strip()
-        except Exception as ex:
-            # host down or deez out 
-            status = "<?xml version=\"1.0\"?><host name=\""+host+"\"><status>RED</status></host>"
-        statxml = ET.XML(status)
-        color = str(statustransform(statxml))
-        if color == "GREEN":
-            greendoc.append(str(hosttransform(statxml)))
-            green += 1
-        elif color == "YELLOW":
-            yellowdoc.append(str(hosttransform(statxml)))
-            yellow += 1
-        elif color == "RED":
-            reddoc.append(str(hosttransform(statxml)))
-            red += 1
-    doc.close()
-    reddoc.append("<red>"+str(red)+"</red>"+"<yellow>"+str(yellow)+"</yellow>"+"<green>"+str(green)+"</green>"+"</hosts>")
-    yellowdoc.append("<red>"+str(red)+"</red>"+"<yellow>"+str(yellow)+"</yellow>"+"<green>"+str(green)+"</green>"+"</hosts>")
-    greendoc.append("<red>"+str(red)+"</red>"+"<yellow>"+str(yellow)+"</yellow>"+"<green>"+str(green)+"</green>"+"</hosts>")
-    return([red,"".join(reddoc),yellow,"".join(yellowdoc),green,"".join(greendoc)])
+	doc = open('/usr/share/deez/deezhosts', 'r')
+	for line in doc:	
+		host = line.strip('\n')
+		url = protocol+host+":"+str(port)+path
+		try:
+			response = http.request('GET', url)
+			# checkDeez decides if GREEN or YELLOW
+			status = response.data.strip() 
+		except Exception as ex:
+			# host down or deez out 
+			status = "<?xml version=\"1.0\"?><host name=\""+host+"\"><status>RED</status></host>"
+		statxml = ET.XML(status)
+		color = str(statustransform(statxml))
+		if color == "GREEN":
+			greendoc.append(str(hosttransform(statxml)))
+			green += 1
+		elif color == "YELLOW":
+			yellowdoc.append(str(hosttransform(statxml)))
+			yellow += 1
+		elif color == "RED":
+			reddoc.append(str(hosttransform(statxml)))
+			red += 1
+	doc.close()
+	reddoc.append("<red>"+str(red)+"</red>"+"<yellow>"+str(yellow)+"</yellow>"+"<green>"+str(green)+"</green>"+"</hosts>")
+	yellowdoc.append("<red>"+str(red)+"</red>"+"<yellow>"+str(yellow)+"</yellow>"+"<green>"+str(green)+"</green>"+"</hosts>")
+	greendoc.append("<red>"+str(red)+"</red>"+"<yellow>"+str(yellow)+"</yellow>"+"<green>"+str(green)+"</green>"+"</hosts>")
+	return([red,"".join(reddoc),yellow,"".join(yellowdoc),green,"".join(greendoc)])
 
 docs = sweepDeez()
 if docs[0] > 0:
-    bg = "red"
+	bg = "red"
 elif docs[2] > 0:
-    bg = "yellow"
+	bg = "yellow"
 else:
-    bg = "green"
+	bg = "green"
 
 rygdoc = "<?xml version=\"1.0\"?><sums><bg>"+bg+"</bg><red>"+str(docs[0])+"</red><yellow>"+str(docs[2])+"</yellow><green>"+str(docs[4])+"</green></sums>"
 rygxml = ET.XML(rygdoc)
